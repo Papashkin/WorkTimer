ffbe703ffcaae4872c4d088e702a47b892750738
Papashkin <Papashkin-90@yandex.ru>
2018-02-15 19:08:38 +0300

    Work timer for 8-hour day (countdown)

---
+++ src/MainTimer.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class MainTimer {
+
+    public static void main(String args[]) {
+        TimerGUI mainScreen = new TimerGUI();
+        mainScreen.setTitle("Work timer");
+        mainScreen.prepare();
+        mainScreen.pack();
+        mainScreen.setSize(200,180);
+        mainScreen.setVisible(true);
+        mainScreen.setResizable(false);
+        mainScreen.setDefaultCloseOperation(mainScreen.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/TimerGUI.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
+import static java.lang.String.format;
+
+public class TimerGUI extends JFrame {
+    private JPanel panel;
+    private JButton bStart;
+    private JButton bAgain;
+    private JButton bPause;
+    private JLabel lTimer;
+    private long hour, min, sec;
+    JOptionPane finishMsg = new JOptionPane();
+    private String str;
+    private Timer timer = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            sec -= 1;
+
+            if (sec == -1) sec = 59;
+
+            if (sec == 59) {
+                min -= 1;
+                if (min == -1) min = 59;
+                if (min == 59) hour -= 1;
+            }
+
+            if (hour == 0) {
+                timer.stop();
+                finishMsg.showMessageDialog(null, "Work day is finished", "Finish!", JOptionPane.INFORMATION_MESSAGE);
+            }
+            update();
+        }
+    });
+
+    public TimerGUI() {
+        this.getContentPane().add(panel);
+        bStart.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed (ActionEvent e){
+            timer.start();
+        }
+    });
+
+        bPause.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed (ActionEvent e){
+            timer.stop();
+        }
+    });
+
+        bAgain.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed (ActionEvent e){
+            timer.stop();
+            hour = 8;
+            sec = 0;
+            min = 0;
+            update();
+        }
+    });
+}
+
+    public void prepare() {
+        hour = 8;
+        min = 0;
+        sec = 0;
+        str = format("%02d : %02d : %02d", hour, min, sec);
+        lTimer.setText(str);
+    }
+
+    public void update(){
+        str = format("%02d : %02d : %02d", hour, min, sec);
+        lTimer.setText(str);
+    }
+}
